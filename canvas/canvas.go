e8f01a228ff38445675feb786cd30f381a0dc7e9
Sandesh Chapagain <chapagainsandesh88@gmail.com>
2024-07-27 00:05:08 -0500

    add type defs to same file and add ToPPM func

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -2,9 +2,28 @@ package canvas
 
 import (
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
 
 	"github.com/schapagain/raytracer/errors"
 )
+
+type Color struct {
+	R, G, B, A float64
+}
+
+type Canvas interface {
+	Width() int
+	Height() int
+	PixelAt(int, int) (Color, error)
+	SetPixelAt(int, int, Color) error
+}
+
+type canvas struct {
+	width, height int
+	buffer        []Color
+}
 
 // Width returns the width of the canvas c
 func (c *canvas) Width() int {
@@ -29,13 +48,42 @@ func (c *canvas) PixelAt(x, y int) (Color, error) {
 // SetPixelAt sets the color of the pixel at (x,y) on the canvas c
 //
 // It returns an error if location (x,y) is out of bounds
-func (c *canvas) SetPixelAt(x, y int, color Color) (bool, error) {
-
+func (c *canvas) SetPixelAt(x, y int, color Color) error {
 	if x >= c.width || y >= c.height {
-		return false, &errors.OutOfBoundsError{Details: fmt.Sprintf("Invalid location (%d,%d)", x, y)}
+		return &errors.OutOfBoundsError{Details: fmt.Sprintf("Invalid location (%d,%d)", x, y)}
 	}
 	c.buffer[y*c.width+x] = color
-	return true, nil
+	return nil
+}
+
+// ToPPM creates a new PPM format from the current canvas pixel data
+func (c *canvas) ToPPM() PPM {
+
+	maxPPMLineLength := 70
+	maxPixelCharLength := len(strconv.Itoa(MaxColorValue))*3 + 3
+	numPixelsInPPMLine := int(math.Floor(float64(maxPPMLineLength / maxPixelCharLength)))
+	numPPMLines := int(math.Ceil(float64(len(c.buffer) / int(numPixelsInPPMLine))))
+
+	ppmLines := make([]string, numPPMLines)
+	idx := 0
+	for {
+		if idx >= numPPMLines {
+			break
+		}
+		currPPMLinePixels := c.buffer[idx*numPixelsInPPMLine : idx*numPixelsInPPMLine+numPixelsInPPMLine]
+		currPPMLine := strings.Builder{}
+		for i := range currPPMLinePixels {
+			color := currPPMLinePixels[i]
+			r := int(math.Max(math.Min(float64(MaxColorValue), color.R*float64(MaxColorValue)), 0))
+			g := int(math.Max(math.Min(float64(MaxColorValue), color.G*float64(MaxColorValue)), 0))
+			b := int(math.Max(math.Min(float64(MaxColorValue), color.B*float64(MaxColorValue)), 0))
+			currPPMLine.WriteString(fmt.Sprintf("%d %d %d ", r, g, b))
+		}
+		ppmLines = append(ppmLines, currPPMLine.String())
+		idx++
+	}
+	p := NewPPM(c.height, c.width, MaxColorValue, &ppmLines)
+	return p
 }
 
 // NewCanvas returns an empty canvas initialized with Color{0,0,0,0}
@@ -47,7 +95,7 @@ func NewCanvas(width, height int) Canvas {
 // String returns the string representation
 // of c
 func (c Color) String() string {
-	return fmt.Sprintf("(%f,%f,%f,%f)", c.R, c.G, c.B, c.A)
+	return fmt.Sprintf("(%.3f,%.3f,%.3f,%.3f)", c.R, c.G, c.B, c.A)
 }
 
 // Add add each component of c1 and c2 and
@@ -70,6 +118,6 @@ func (c1 Color) Multiply(c2 Color) Color {
 
 // Multiply multiplies each component of c1 with s and
 // returns the resulting color
-func (c1 Color) Scale(s float32) Color {
+func (c1 Color) Scale(s float64) Color {
 	return Color{c1.R * s, c1.G * s, c1.B * s, c1.A * s}
 }
